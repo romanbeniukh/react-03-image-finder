960f68dbe86e510f845d062b89dafb9e5e3e03e3
Roman Beniukh <ROMAN.BENIUKH@GMAIL.COM>
2020-04-13 11:53:49 +0300

    Updates

--- precache-manifest.80e8421ce250f90224d5140692f8e505.js
+++ precache-manifest.80e8421ce250f90224d5140692f8e505.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0d834ea0321a1798d2172f7e79e98c27",
+    "revision": "5c69eff9e8199ee2efee6f1a91b08734",
     "url": "/react-03-image-finder/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-03-image-finder/static/css/2.f87e9f44.chunk.css"
   },
   {
-    "revision": "36f3fc6ae84570ccbfa4",
-    "url": "/react-03-image-finder/static/css/main.2db1f40f.chunk.css"
+    "revision": "b6eb252ce6134f8800be",
+    "url": "/react-03-image-finder/static/css/main.81f0524e.chunk.css"
   },
   {
     "revision": "403373f1a70a01772158",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-03-image-finder/static/js/2.f524296f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "36f3fc6ae84570ccbfa4",
+    "revision": "b6eb252ce6134f8800be",
     "url": "/react-03-image-finder/static/js/main.2ebb9e73.chunk.js"
   },
   {
